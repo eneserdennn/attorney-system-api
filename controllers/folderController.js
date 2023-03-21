ee00e97ea641e4f1a6827d72b56dffb94432c69f
orkunoci <devorkunoci@gmail.com>
2023-03-21 15:59:11 +0300

    folder controller

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,94 +1,134 @@
-const Folder = require('../models/folderModel');
+const Folder = require("../models/folderModel");
 
 // Get all folders for a client
 const getClientFolders = async (req, res) => {
-    try {
-        const folders = await Folder.find({ clientId: req.params.clientId });
-        res.status(200).json(folders);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
-    }
+  try {
+    const folders = await Folder.find(req.params.clientId);
+    res.status(200).json(folders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 // Create a new folder for a client
 const createClientFolder = async (req, res) => {
-    try {
-        const { folderName, nature, jurisdictions, currency, language, billingMethod, applicableRate, budgetedAmount, fixedExpenses, documentsPath } = req.body;
-        const newFolder = new Folder({
-            folderName,
-            nature,
-            jurisdictions,
-            currency,
-            language,
-            userId: req.user._id,
-            clientId: req.params.clientId,
-            billingMethod,
-            applicableRate,
-            budgetedAmount,
-            fixedExpenses,
-            documentsPath
-        });
-        const savedFolder = await newFolder.save();
-        res.status(201).json(savedFolder);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
-    }
+  try {
+    const {
+      folderName,
+      nature,
+      jurisdictions,
+      currency,
+      language,
+      billingMethod,
+      applicableRate,
+      budgetedAmount,
+      fixedExpenses,
+      documentsPath,
+      userId,
+      clientId,
+    } = req.body;
+    const newFolder = new Folder({
+      folderName,
+      nature,
+      jurisdictions,
+      currency,
+      language,
+      userId,
+      clientId,
+      billingMethod,
+      applicableRate,
+      budgetedAmount,
+      fixedExpenses,
+      documentsPath,
+    });
+    const savedFolder = await newFolder.save();
+    res.status(201).json(savedFolder);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 // Get a single folder for a client
 const getClientFolder = async (req, res) => {
-    try {
-        const folder = await Folder.findOne({ _id: req.params.id, clientId: req.params.clientId });
-        if (!folder) {
-            return res.status(404).json({ message: 'Folder not found' });
-        }
-        res.status(200).json(folder);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
+  try {
+    const folder = await Folder.findOne({
+      _id: req.params.id,
+      clientId: req.params.clientId,
+    });
+    if (!folder) {
+      return res.status(404).json({ message: "Folder not found" });
     }
+    res.status(200).json(folder);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 // Update a folder for a client
 const updateClientFolder = async (req, res) => {
-    try {
-        const { folderName, nature, jurisdictions, currency, language, billingMethod, applicableRate, budgetedAmount, fixedExpenses, documentsPath } = req.body;
-        const updatedFolder = await Folder.findOneAndUpdate({ _id: req.params.id, clientId: req.params.clientId }, {
-            folderName,
-            nature,
-            jurisdictions,
-            currency,
-            language,
-            billingMethod,
-            applicableRate,
-            budgetedAmount,
-            fixedExpenses,
-            documentsPath
-        }, { new: true });
-        if (!updatedFolder) {
-            return res.status(404).json({ message: 'Folder not found' });
-        }
-        res.status(200).json(updatedFolder);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
+  try {
+    const {
+      folderName,
+      nature,
+      jurisdictions,
+      currency,
+      language,
+      billingMethod,
+      applicableRate,
+      budgetedAmount,
+      fixedExpenses,
+      documentsPath,
+    } = req.body;
+    const updatedFolder = await Folder.findOneAndUpdate(
+      { _id: req.params.id, clientId: req.params.clientId },
+      {
+        folderName,
+        nature,
+        jurisdictions,
+        currency,
+        language,
+        billingMethod,
+        applicableRate,
+        budgetedAmount,
+        fixedExpenses,
+        documentsPath,
+      },
+      { new: true }
+    );
+    if (!updatedFolder) {
+      return res.status(404).json({ message: "Folder not found" });
     }
+    res.status(200).json(updatedFolder);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 // Delete a folder for a client
 const deleteClientFolder = async (req, res) => {
-    try {
-        const deletedFolder = await Folder.findOneAndDelete({ _id: req.params.id, clientId: req.params.clientId });
-        if (!deletedFolder) {
-            return res.status(404).json({ message: 'Folder not found' });
-        }
-        res.status(200).json(deletedFolder);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server Error' });
+  try {
+    const deletedFolder = await Folder.findOneAndDelete({
+      _id: req.params.id,
+      clientId: req.params.clientId,
+    });
+    if (!deletedFolder) {
+      return res.status(404).json({ message: "Folder not found" });
     }
+    res.status(200).json(deletedFolder);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
-module.exports = { getClientFolders, createClientFolder, getClientFolder, updateClientFolder, deleteClientFolder };
+module.exports = {
+  getClientFolders,
+  createClientFolder,
+  getClientFolder,
+  updateClientFolder,
+  deleteClientFolder,
+};
--- server.js
+++ server.js
@@ -1,27 +1,25 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const colors = require('colors');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const cors = require("cors");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
-const { errorHandler } = require('./middleware/errorMiddleware');
-
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 connectDB();
 
 const app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use('/api/clients', require('./routes/clientRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/send', require('./routes/mailRoutes'));
+app.use("/api/clients", require("./routes/clientRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+// app.use('/api/send', require('./routes/mailRoutes'));
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
